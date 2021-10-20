bbfeb77cfd762c20b86526e81fac39dc38439aee
Tiago França <dev@tiagofranca.com>
2021-10-20 09:36:45 -0300

    No ramo master Mudanças a serem submetidas: 	new file:   .gitignore 	new file:   06-lacos-de-repeticao/06-02-for-foreach-while.php

---
+++ 06-lacos-de-repeticao/06-02-for-foreach-while.php
@@ -0,0 +1,48 @@
+<?php
+
+$verduras = [
+    'Arroz',
+    'Feijão',
+    'Salada',
+    'tomate',
+    'cebola',
+    'alfaçe',
+    'Rucula',
+    'cebolinha',
+];
+
+// echo "- ". $verduras[0].PHP_EOL;
+// echo "- ". $verduras[1].PHP_EOL;
+// echo "- ". $verduras[2].PHP_EOL;
+// echo "- ". $verduras[3].PHP_EOL;
+// echo "- ". $verduras[4].PHP_EOL;
+// echo "- ". $verduras[5].PHP_EOL;
+
+echo PHP_EOL."------------- for ------------------".PHP_EOL;
+
+$total_de_itens = count($verduras);
+for (
+    /*A*/ $i = 0;
+    /*B*/ $i < $total_de_itens;
+    /*D*/ $i = $i +1
+)
+{
+    /*C*/ echo "- ". $verduras[$i].PHP_EOL;
+}
+
+echo PHP_EOL."------------- foreach ------------------".PHP_EOL;
+
+foreach ($verduras as $value)
+{
+    echo "- ".$value.PHP_EOL;
+}
+
+echo PHP_EOL."------------ while -------------------".PHP_EOL;
+
+$i = 0;
+$total = count($verduras);
+
+while ($i < $total)
+{
+    echo "- ". $verduras[$i++].PHP_EOL;
+}
